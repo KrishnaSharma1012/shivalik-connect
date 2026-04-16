c853ad549ee9d2eb14f604ab92b8d395beca64fc
PrakharTagra <prakhartagra16@gmail.com>
2026-04-16 17:12:36 +0530

    DB

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import connectDB from "./config/database.js";
 
@@ -16,7 +18,13 @@ import connectionRoutes from "./routes/Connection.js";
 import earningRoutes from "./routes/Earning.js";
 import adminRoutes from "./routes/Admin.js";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, ".env"),
+  override: true,
+});
 
 const app = express();
 
